8e0419829e550df3ae858b8c73ac4749ebc95f3f
marlon-frazao <frazao.marlon@gmail.com>
2021-06-22 10:56:00 -0300

    Database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
+import 'reflect-metadata';
 import express from 'express';
 
+import './database';
+
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("Olá NLW");
-});
-
-app.post("/test", (request, response) => {
-    return response.send("Olá NLW método post");
-});
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
